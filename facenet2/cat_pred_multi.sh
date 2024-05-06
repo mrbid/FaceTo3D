194168819ace341817a13caf139661bda23fb6ee
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-05-06 11:42:14 +0100

    Update cat_pred_multi.sh

--- facenet2/cat_pred_multi.sh
+++ facenet2/cat_pred_multi.sh
@@ -1,5 +1,12 @@
-mkdir pred_multi_final
+mkdir -p pred_multi_final
+rm -rf pred_multi_final/*
 for i in {0..32768}; do
-    cat pred_multi/train_y_$i.dat >> pred_multi_final/pred.dat
-    cat pred_multi/train_y_$i.csv >> pred_multi_final/pred.csv
+    if [ -f 'pred_multi/train_y_'$i'.dat' ]; then
+        cat pred_multi/train_y_$i.dat >> pred_multi_final/pred.dat
+        cat pred_multi/train_y_$i.csv >> pred_multi_final/pred.csv
+    else
+        echo 'File not found: pred_multi/train_y_'$i'.dat'
+        echo 'Sequence broken, exiting. (incomplete prediction may likely exist)'
+        exit 0
+    fi
 done
