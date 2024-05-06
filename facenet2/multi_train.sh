50a252df1199720822c30b594e5336e076c36301
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-05-06 11:42:56 +0100

    Update multi_train.sh

--- facenet2/multi_train.sh
+++ facenet2/multi_train.sh
@@ -1,10 +1,13 @@
 p=0
 for i in {0..32768}; do
-	nohup xterm -e 'python3 fit.py train_y/train_y_'$i'.dat' > /dev/null 2>&1 &
-    echo 'python3 fit.py train_y/train_y_'$i'.dat'
-    p=$((p+1))
-    if ((p >= 24)); then
-        p=0
-        read -p "Any key to continue... " > /dev/null
+    if [ ! -f 'models/train_y_'$i'.keras' ]; then
+	python3 fit.py 'train_y/train_y_'$i'.dat' > /dev/null 2>&1 &
+        echo 'python3 fit.py train_y/train_y_'$i'.dat'
+        p=$((p+1))
+        if ((p >= 42)); then
+            p=0
+            date
+            while wait -f %% 2>/dev/null; do :; done
+        fi
     fi
 done;
