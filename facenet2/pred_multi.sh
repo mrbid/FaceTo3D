7553bb56f2b3e5d4c0538343cb21fac0abc0ee65
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-05-06 17:28:07 +0100

    Update pred_multi.sh

--- facenet2/pred_multi.sh
+++ facenet2/pred_multi.sh
@@ -1,5 +1,5 @@
 p=0
-for i in {0..32768}; do
+for i in {0..32767}; do
     if [ -f 'models/train_y_'$i'.keras' ]; then
         if [ ! -f 'pred_multi/train_y_'$i'.dat' ]; then
             python3 pred_multi.py 'models/train_y_'$i'.keras' > /dev/null 2>&1 &
